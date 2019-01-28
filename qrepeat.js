697142cc173576150456d38ad3336e168c151538
Andras <andras@andrasq.com>
2019-01-28 12:36:51 -0500

    initial version

---
+++ qrepeat.js
@@ -0,0 +1,135 @@
+/**
+ * very fast repeatUntil
+ * Adapted from microrest, which adapted it from aflow, originally called repeatUntil.
+ *
+ * Copyright (C) 2019 Andras Radics
+ * Licensed under the Apache License, Version 2.0
+ *
+ * 2019-01-25 - AR.
+ */
+
+'use strict';
+
+var util = require('util');
+
+var untilSingleton = new QRepeat();
+var whileSingleton = new QRepeat();
+
+var qrepeat = module.exports;
+qrepeat.QRepeat = QRepeat;
+qrepeat.repeatUntil = function(fn, cb) { untilSingleton.repeatUntil(fn, cb) };
+qrepeat.repeatUntilA = function(fn, arg, testStop, cb) { new QRepeat().repeatUntil(fn, arg, testStop, cb) };
+qrepeat.doUntil = qrepeat.repeatUntil;
+qrepeat.repeatWhile = function(ck, fn, cb) { whileSingleton.repeatWhile(ck, fn, cb) };
+qrepeat.cbAlreadyCalledWarning = 'qrepeat: callback already called';
+qrepeat.cbThrewWarning = 'qrepeat: callback threw';
+qrepeat = toStruct(qrepeat);
+
+// node-v0.10 nextTick did not accept function args yet
+var nodeVersion = process.version.slice(1, process.version.indexOf('.'));
+var nextTick = nodeVersion >= 4 ? process.nextTick : setImmediate;
+
+// TODO: move into the class, add mechanism for turning off warnings 
+// print a warning to stderr
+function warn( ) {
+    console.warn("%s -- qrepeat: %s", new Date().toISOString(), util.format.apply(util, [].slice.call(arguments)));
+}
+
+function makeError(code, message) {
+    var err = (typeof message === 'string') ? (err = new Error(message), err.code = code, err) : err;
+    err.qrcode = code;
+    Error.captureStackTrace(err, makeError);
+    return err;
+}
+
+function QRepeat( ) {
+}
+
+QRepeat.prototype._repeat = function _repeat( loop, arg, callback ) {
+    var self = this;
+    self.arg = arg;
+    var depth = 0, tickBreaks = 0, callCount = 0, returnCount = 0;
+    // self._testStop must have been set before calling
+
+    callCount++; self._tryCall(loop, _return);
+
+    function _return(err, stop) {
+        // TODO: maybe _tryCallback should suppress duplicate callbacks to the caller
+        // NOTE: counting callbacks is not fool proof:
+        //   - a duplicate callback before current call returns will be used instead
+        //   - a duplicate callback could be used instead of a missing callback
+        if (++returnCount > callCount) {
+            // warn in addition to invoking callback with an error
+            var msg; warn((msg = qrepeat.cbAlreadyCalledWarning + (err ? '; new error: ' + err.stack : '')));
+            if (err) err.qrcode = 'DUPCB';
+            self._tryCallback(callback, err || makeError('DUPCB', msg));
+        }
+        else if (self._testStop(err, stop, arg)) { return self._tryCallback(callback, err) }
+        else if (depth++ < 20) { callCount++; return self._tryCall(loop, _return) }
+        else {
+            // every 20 calls break up the call stack, every 1000 yield to the event loop
+            depth = 0; callCount++;var fn = function(){ self._tryCall(loop, _return) }
+            if (++tickBreaks > 50) { tickBreaks = 0; setImmediate(fn) } else { nextTick(fn) }
+        }
+    }
+}
+
+QRepeat.prototype._repeatRU = cloneFunc(QRepeat.prototype._repeat);
+QRepeat.prototype.repeatUntil = function repeatUntil( loopFunc, callback ) {
+    this._testStop = this._testRepeatUntilDone;
+    this._repeatRU(loopFunc, 0, callback);
+};
+// using our own copy of _repeat raises the deoptimized throughput from 34 to 37m/s (or 37 to 48m/s reusing the test func)
+QRepeat.prototype._repeatRW = cloneFunc(QRepeat.prototype._repeat);
+QRepeat.prototype.repeatWhile = function repeatWhile( testFunc, loopFunc, callback ) {
+    // this function must be called on a new object every time
+    var state = { test: testFunc, loop: loopFunc, cb: callback };
+//    this._tryCall = this._tryCall2;
+//    this._tryCallback = this._tryCallback2;
+//    this._testStop = this._whileTestDone;
+    this._testStop = this._noop;
+    this._testWhile = testFunc;
+    this._tryCall = this._tryCallWhile;
+    this._repeatRW(this._whileLooper, state, this._cleanCallback);
+}
+QRepeat.prototype.repeatWhile = function repeatWhile( testFunc, loopFunc, callback ) {
+    // this function must be called on a new object every time
+    var state = { test: testFunc, loop: loopFunc, cb: callback };
+    this._tryCall = this._tryCall2;
+    this._tryCallback = this._tryCallback2;
+    this._testStop = this._whileTestDone;
+    this._repeatRW(this._whileLooper, state, this._cleanCallback);
+}
+QRepeat.prototype._whileLooper = function _whileLooper( done, state ) { state.test() ? state.loop(done) : done(state, true) }
+QRepeat.prototype._whileTestDone = function _whileTestDone( err, done, state ) { return err === state ? done : err }    // only believe `done` if err==state from _whileLooper
+QRepeat.prototype._noop = function(){};
+QRepeat.prototype._testWhile = function(){};
+QRepeat.prototype._tryCallWhile = function _tryCallWhile( func, cb ) {
+    if (this._testWhile()) this._tryCall1(func, cb);
+    else cb(null, true);
+}
+
+QRepeat.prototype._tryCall1 = function _tryCall1(func, cb) {
+    try { func(cb) } catch (err) { cb(err || makeError('THREW', 'threw falsy error')) } };
+QRepeat.prototype._tryCall2 = function _tryCall2(func, cb) {
+    try { func(cb, this.arg) } catch (err) { cb(err || makeError('THREW', 'threw falsy error'), this.arg) } };
+QRepeat.prototype._tryCallback1 = function _tryCallback1(cb, err) {
+    try { cb(err) } catch (err2) { warn(qrepeat.cbThrewWarning, err2, err2.stack); throw err2 } }
+QRepeat.prototype._tryCallback2 = function _tryCallback2(cb, err) {
+    try { cb(err, this.arg) } catch (err2) { warn(qrepeat.cbThrewWarning, err2); throw err2 } }
+QRepeat.prototype._cleanCallback = function _cleanCallback( err, state ) {
+    err === state ? state.cb() : state.cb(err) }
+QRepeat.prototype._testRepeatUntilDone = function _testRepeatUntilDone(err, done) {
+    return err || done; }
+QRepeat.prototype._testStop = QRepeat.prototype._testRepeatUntilDone;
+QRepeat.prototype._stopFlag = false;
+QRepeat.prototype._testStopFlag = function() { return this._stopFlag };
+QRepeat.prototype._noop = function(){};
+
+QRepeat.prototype._tryCall = QRepeat.prototype._tryCall1;
+QRepeat.prototype._tryCallback = QRepeat.prototype._tryCallback1;
+
+QRepeat.prototype = toStruct(QRepeat.prototype);
+function toStruct(obj) { return eval("toStruct.prototype = obj") }
+function cloneFunc(fn) { var x; eval("x = " + fn.toString()); return x }
+
---
+++ test-qrepeat.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var qrepeat = require('./');
+
+module.exports = {
+    'should export expected functions': function(t) {
+        t.equal(typeof qrepeat.repeatUntil, 'function');
+        t.equal(typeof qrepeat.repeatWhile, 'function');
+        t.done();
+    },
+
+    'repeatUntil': {
+        'should repeat at least 1 time': function(t) {
+            var n = 0;
+            qrepeat.repeatUntil(function(cb) { cb(null, ++n >= 0) }, function(err) {
+                t.equal(n, 1);
+                t.done(err);
+            })
+        },
+
+        'should repeat 1 times time': function(t) {
+            var n = 0;
+            qrepeat.repeatUntil(function(cb) { cb(null, ++n >= 1) }, function(err) {
+                t.equal(n, 1);
+                t.done(err);
+            })
+        },
+
+        'should repeat 1000000 times': function(t) {
+            var n = 0;
+            qrepeat.repeatUntil(function(cb) { cb(null, ++n >= 1e6) }, function(err) {
+                t.equal(n, 1e6);
+                t.done(err);
+            })
+        },
+
+        'errors': {
+            'should break on and return errors': function(t) {
+                var n = 0, error = new Error('test error');
+                qrepeat.repeatUntil(function(cb) {
+                    n += 1;
+                    cb(n === 3 && error, false);
+                }, function(err) {
+                    t.equal(n, 3);
+                    t.equal(err, error);
+                    t.done();
+                })
+            },
+
+            'should print warning and return error on duplicate callback': function(t) {
+                var n = 0, rets = [];
+                var stub = t.stub(process.stderr, 'write');
+                qrepeat.repeatUntil(function(cb) {
+                    n += 1;
+                    setImmediate(cb, null, n >= 10);
+                    if (n == 3) setTimeout(cb, 2);
+                    if (n == 7) setTimeout(cb, 2, new Error('test error'));
+                }, function(err) {
+                    rets.push(err);
+                    if (rets.length === 3) {
+                        stub.restore();
+                        t.equal(rets.length, 3);
+                        t.ok(rets[0] == null);
+                        t.ok(rets[1] instanceof Error);
+                        t.ok(rets[2] instanceof Error);
+                        t.equal(stub.args.length, 2);
+                        t.contains(stub.args[0][0], /callback already called/);
+                        t.contains(stub.args[1][0], /callback already called/);
+                        // returns a new error on duplicate callback
+                        t.contains(rets[1].message, /already called/);
+                        // but returns the actual error if callback had error
+                        t.contains(rets[2].message, /test error/);
+                        t.done();
+                    }
+                })
+            },
+
+            'should print warning and rethrow if user callback throws': function(t) {
+                var stub = t.stub(process.stderr, 'write');
+                var listeners = process.listeners('uncaughtException');
+                // suppress global error handlers to not kill this test
+                for (var i=0; i<listeners.length; i++) process.removeListener('uncaughtException', listeners[i]);
+                process.once('uncaughtException', function errListener(err2) {
+                    t.equal(err2.message, 'test error');
+                    for (var i=0; i<listeners.length; i++) process.on('uncaughtException', listeners[i]);
+                    t.done();
+                })
+                qrepeat.repeatUntil(function(cb) { return cb(null, true) }, function(err) {
+                    setTimeout(function() { throw new Error('test error') }, 10);
+                })
+            },
+        },
+    },
+
+    'repeatWhile': {
+        'should repeat 0 times': function(t) {
+            var n = 0;
+            qrepeat.repeatWhile(function() { return false }, function(cb) { n++; cb() }, function(err) {
+                t.equal(n, 0);
+                t.done();
+            })
+        },
+
+        'should repeat 1 times': function(t) {
+            var n = 0;
+            qrepeat.repeatWhile(function() { return n < 1 }, function(cb) { n++; cb() }, function(err) {
+                t.equal(n, 1);
+                t.done();
+            })
+        },
+
+        'should repeat 1000000 times': function(t) {
+            var n = 0;
+            qrepeat.repeatWhile(function() { return n < 1e6 }, function(cb) { n++; cb() }, function(err) {
+                t.equal(n, 1e6);
+                t.done();
+            })
+        },
+
+        'errors': {
+        },
+    },
+}
